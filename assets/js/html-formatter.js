61f3f5703fe5ff813c488d087eeb222d65832f2b
Mohithoteldekho123 <148522843+hoteldekho123@users.noreply.github.com>
2025-01-18 11:51:25 +0530

    df

--- assets/js/html-formatter.js
+++ assets/js/html-formatter.js
@@ -1,34 +1,10 @@
- // Function to format HTML code
- function formatHTML(html) {
-    let formatted = '';
-    let indentLevel = 0;
-    const indentSize = 4; // Number of spaces for each indent
+function formatHTML() {
+    let htmlInput = document.getElementById('htmlInput').value;
+    let formattedHTML = htmlInput
+        .replace(/>\s+</g, '>\n<')        // Add line breaks
+        .replace(/\s{2,}/g, ' ')         // Remove extra spaces
+        .replace(/</g, '&lt;')           // Escape HTML for output
+        .replace(/>/g, '&gt;');
 
-    // Add indentation and line breaks based on tag structure
-    html.split(/>\s*</).forEach(element => {
-        if (element.match(/^\/\w/)) {
-            // Closing tag, reduce indent
-            indentLevel -= 1;
-        }
-
-        formatted += ' '.repeat(indentSize * indentLevel) + '<' + element + '>\n';
-
-        if (element.match(/^<?\w[^>]*[^\/]$/) && !element.includes('!')) {
-            // Opening tag or incomplete self-closing tag, increase indent
-            indentLevel += 1;
-        }
-    });
-
-    return formatted.trim();
+    document.getElementById('formattedHTML').innerText = formattedHTML;
 }
-
-// Attach event listener to the "Format HTML" button
-document.getElementById('formatBtn').addEventListener('click', () => {
-    const htmlInput = document.getElementById('html-input').value;
-
-    // Format the input HTML
-    const formattedHTML = formatHTML(htmlInput);
-
-    // Display the formatted HTML in the output div
-    document.getElementById('formatted-html').textContent = formattedHTML;
-});
